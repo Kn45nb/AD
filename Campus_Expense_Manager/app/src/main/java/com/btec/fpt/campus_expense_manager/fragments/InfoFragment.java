d97971c1424f9e399da08f0fc63a64ed6d1fdbb7
Kiên <110784389+Kn45nb@users.noreply.github.com>
2025-04-18 12:08:04 +0700

    .

---
+++ Campus_Expense_Manager/app/src/main/java/com/btec/fpt/campus_expense_manager/fragments/InfoFragment.java
@@ -0,0 +1,17 @@
+package com.btec.fpt.campus_expense_manager.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.btec.fpt.campus_expense_manager.R;
+
+public class InfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
