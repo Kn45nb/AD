25cae3f77daa9121085d1ed7ce81637e3495205e
Kiên <110784389+Kn45nb@users.noreply.github.com>
2025-04-18 12:18:55 +0700

    Changer Infomation User

---
+++ Campus_Expense_Manager/app/src/main/java/com/btec/fpt/campus_expense_manager/fragments/ChangerProfileFragment.java
@@ -0,0 +1,19 @@
+package com.btec.fpt.campus_expense_manager.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.btec.fpt.campus_expense_manager.R;
+
+public class ChangerProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Hiện tại để trống, chỉ trả về layout rỗng
+        // Kn45nb
+        return inflater.inflate(R.layout.fragment_changer_profile, container, false);
+    }
+}
